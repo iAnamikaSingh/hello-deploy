8d494ef45757283eecdcdd7dcd6b323cc199bda4
Anamika Singh <214621661+iAnamikaSingh@users.noreply.github.com>
2026-03-25 22:17:34 +0530

    fix: added dynamic port for development

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -5,5 +6,5 @@ app.use(cors());
 app.get("/", (req,res) => {
     res.json({message: "Hi, I am Anamika"});
 });
-const port = 3000;
-app.listen(port, () => console.log(`Server is running on the port ${port}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on the port ${PORT}`));
